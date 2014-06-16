d33fc9eb70022f0353a7435dc14334b5dfd224ed
nearffxx <nearffxx@hotmail.com>
2014-06-16 16:29:21 +0200

    rspec

--- spec/requests/test_pages_spec.rb
+++ spec/requests/test_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "TestPages", :type => :request do
+describe "TestPages", :type => :request do
   describe "GET /test_pages" do
     it "works! (now write some real specs)" do
-      get test_pages_index_path
-      expect(response.status).to be(200)
+      visit "/home"
+      page.should have_content('skuola42')
     end
   end
 end
